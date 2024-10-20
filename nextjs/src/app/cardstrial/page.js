29822205f43d824eef8ac9621a1748cbdfa8a38a
Aaditya Paudel <78781844+adityaspaudel@users.noreply.github.com>
2024-10-20 09:53:06 +0545

    react icons installed + shopping cart made

---
+++ nextjs/src/app/cardstrial/page.js
@@ -0,0 +1,12 @@
+import Card from '@/components/cards/page'
+import React from 'react'
+
+const Cardtrial = () => {
+  return (
+    <div>
+      <Card/>
+    </div>
+  )
+}
+
+export default Cardtrial
